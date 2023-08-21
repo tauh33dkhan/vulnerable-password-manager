1b7f9e48a9436cf49cf2a591f34b19d5d11f4d37
Tauheed Khan <43419559+tauh33dkhan@users.noreply.github.com>
2023-08-21 18:42:03 +0530

    push to git

---
+++ popup.js
@@ -0,0 +1,32 @@
+const clearPasswordsBtn = document.getElementById("clear-passwords-btn");
+let passwordsDiv = document.getElementById("passwords-div");
+
+clearPasswordsBtn.addEventListener("click", () => {
+    chrome.storage.sync.set({ passwords: [] });
+    passwordsDiv.remove();
+    passwordsDiv = document.createElement("div");
+    document.body.appendChild(passwordsDiv);
+})
+
+const getHostname = async () => {
+    const { passwords } = await chrome.storage.sync.get("passwords");
+    passwords.forEach(password => {
+        const linkEl = document.createElement("p");
+        linkEl.innerHTML = "*." + password.hostname;
+        const deleteBtn = document.createElement("button");
+        deleteBtn.innerText = "Delete";
+
+        passwordsDiv.appendChild(linkEl);
+        passwordsDiv.appendChild(deleteBtn);
+        
+        deleteBtn.addEventListener("click", () => {
+            const updatedPasswords = passwords.filter(pwd => pwd !== password);
+            chrome.storage.sync.set({ passwords: updatedPasswords });
+
+            linkEl.remove();
+            deleteBtn.remove();            
+        })
+    })
+}
+
+getHostname();
